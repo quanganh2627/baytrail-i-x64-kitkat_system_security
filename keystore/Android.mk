ccb5fe1d37397a203e9778d3b866a6a5314f35cd
Sun Yijin <yijin.sun@intel.com>
2013-10-11 23:06:27 +0800

    Revert "Integrate Arkham to mainline"
    BZ: 144444
    
    Conflict with google security patch "02d44fe7eb11c09745e633efb3c799325e5d0cb1" [PATCH] Move key name creation to common path
    
    This reverts commit bc321e45c6697b01a19e3c80ef2376391de60685.
    
    Category: feature differentiation
    Domain: AOSP.Extend-Arkham
    Origin: Internal
    Upstream-Candidate: No, proprietary
    
    Change-Id: I078bfbba59a23488b2ad5cdb42c3a16be1fec4df
    Signed-off-by: Rares Petrescu <rares.petrescu@intel.com>
    Reviewed-on: http://android.intel.com:8080/137854
    Reviewed-by: Peng, QingwenX <qingwenx.peng@intel.com>
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Reviewed-by: Yin, Fengwei <fengwei.yin@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- keystore/Android.mk
+++ keystore/Android.mk
@@ -18,9 +18,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wall -Wextra -Werror
-ifeq ($(strip $(INTEL_FEATURE_ARKHAM)),true)
-LOCAL_CFLAGS += -DINTEL_FEATURE_ARKHAM
-endif
 LOCAL_SRC_FILES := keystore.cpp keyblob_utils.cpp
 LOCAL_C_INCLUDES := external/openssl/include
 LOCAL_SHARED_LIBRARIES := libcutils libcrypto libhardware libkeystore_binder libutils liblog libbinder
